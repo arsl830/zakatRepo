1126f55dbca48a666ff212221ba640b2a773010e
arsl830 <m.arslan89343@gmail.com>
2021-10-24 20:48:02 +0500

    eigth commit

---
+++ android app1/Arsalan/app/src/main/java/com/example/arsalan/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.arsalan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
