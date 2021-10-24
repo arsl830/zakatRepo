fc5f097cd17853ed9619b98538725d9c8b47f5a1
arsl830 <m.arslan89343@gmail.com>
2021-10-24 20:49:59 +0500

    ninth commit

--- android app1/Arsalan/app/src/main/java/com/example/arsalan/MainActivity.java
+++ android app1/Arsalan/app/src/main/java/com/example/arsalan/MainActivity.java
@@ -8,3 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView gold, silver, cash, total;
+    Button calculate;
+    float gold_t, silver_t, cash_t;
+    double total_t;
+    
+}
