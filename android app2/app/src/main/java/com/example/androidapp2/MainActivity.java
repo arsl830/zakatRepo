df2dd211815cf224185cefd3c1bf76b9100b39d0
arsl830 <m.arslan89343@gmail.com>
2021-11-03 23:42:13 +0500

    third commmit

--- android app2/app/src/main/java/com/example/androidapp2/MainActivity.java
+++ android app2/app/src/main/java/com/example/androidapp2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.androidapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.add);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text1 = findViewById(R.id.no1);
+                EditText text2 = findViewById(R.id.no2);
+                String x = text1.getText().toString();
+                String y = text2.getText().toString();
+                String z = x+y;
+                TextView text = findViewById(R.id.add);
+                text.setText(z);
+            }
+        });
     }
 }
