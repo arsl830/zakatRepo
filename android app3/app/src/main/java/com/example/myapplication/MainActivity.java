1ed2c542623056d3c0a5a1b0e8b997cd3dc0a328
arsl830 <m.arslan89343@gmail.com>
2021-11-08 00:00:07 +0500

    third

--- android app3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android app3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text1 = findViewById(R.id.text1);
+                String x = text1.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra ("xyz",x);
+                startActivity(intent);
+            }
+        });
     }
 }
--- android app3/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ android app3/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView text1 = findViewById(R.id.t2);
+        Intent intent = getIntent();
+        String val = intent.getStringExtra("xyz");
+        text1.setText(val);
     }
 }
