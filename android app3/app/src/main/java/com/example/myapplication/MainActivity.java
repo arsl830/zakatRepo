ea4e1fdfb639a32ea3ad7d8bbead478526deb768
arsl830 <m.arslan89343@gmail.com>
2021-11-08 00:08:37 +0500

    4

--- android app3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android app3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText text1 = findViewById(R.id.text1);
-                String x = text1.getText().toString();
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                intent.putExtra ("xyz",x);
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+ "03244374171"));
                 startActivity(intent);
+            //    EditText text1 = findViewById(R.id.text1);
+            //    String x = text1.getText().toString();
+            //    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            //    intent.putExtra ("xyz",x);
+            //    startActivity(intent);
             }
         });
     }
